f2faf44792ab4850c1dd689cb3bf1dc5122c6a36
Maarten Smeets | Twinfield <msmeets@TWFNL0494.local>
2016-03-27 12:31:26 +0200

    First commit

---
+++ client/app.ts
@@ -0,0 +1,22 @@
+import {Component, View, NgZone} from 'angular2/core';
+import {Parties} from 'collections/parties'; 
+import {bootstrap} from 'angular2-meteor';
+
+import {PartiesForm} from 'client/parties-form/parties-form';
+ 
+@Component({
+    selector: 'app'
+})
+@View({
+    templateUrl:'client/app.html',
+    directives: [PartiesForm]
+})
+class Socially {
+	parties: Mongo.Cursor<Object>;
+	
+	constructor () {
+		this.parties = Parties.find();
+	}
+}
+ 
+bootstrap(Socially);
---
+++ client/parties-form/parties-form.ts
@@ -0,0 +1,24 @@
+import {Component, View} from 'angular2/core';
+import {FormBuilder, ControlGroup, Validators} from 'angular2/common';
+
+@Component({
+    selector: 'parties-form'
+})
+
+@View({
+    templateUrl: '/client/parties-form/parties-form.html'
+})
+
+export class PartiesForm { 
+	partiesForm:ControlGroup;
+
+	constructor() {
+		var fb = new FormBuilder();
+		
+		this.partiesForm = fb.group({
+			name: ['', Validators.required],
+			description: [''],
+			location: ['', Validators.required]
+		});
+	}
+}
