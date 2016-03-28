204c07ee244021bae767c6504cd3ff154e80e9d9
Maarten Smeets | Twinfield <msmeets@TWFNL0494.local>
2016-03-28 20:58:53 +0200

    Minor changes

--- client/parties-list/parties-list.ts
+++ client/parties-list/parties-list.ts
@@ -2,7 +2,7 @@ import {Component, View} from 'angular2/core';
 import {Parties} from 'collections/parties';
 import {PartiesForm} from 'client/parties-form/parties-form';
 import {RouterLink} from 'angular2/router';
- 
+
 @Component({
     selector: 'parties-list'
 })
@@ -12,12 +12,12 @@ import {RouterLink} from 'angular2/router';
 })
 export class PartiesList {
     parties: Mongo.Cursor<Party>;
- 
+
     constructor() {
         this.parties = Parties.find();
     }
- 
-    removeParty(party) {
+
+    removeParty(party: Party) {
         Parties.remove(party._id);
     }
 }
