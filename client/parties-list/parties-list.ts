239e346cafb569b5100135a7ba8abf75cf5c6895
Maarten Smeets | Twinfield <msmeets@TWFNL0494.local>
2016-03-28 21:26:30 +0200

    Added account UI

--- client/parties-list/parties-list.ts
+++ client/parties-list/parties-list.ts
@@ -2,13 +2,14 @@ import {Component, View} from 'angular2/core';
 import {Parties} from 'collections/parties';
 import {PartiesForm} from 'client/parties-form/parties-form';
 import {RouterLink} from 'angular2/router';
+import {AccountsUI} from 'meteor-accounts-ui';
 
 @Component({
     selector: 'parties-list'
 })
 @View({
     templateUrl: '/client/parties-list/parties-list.html',
-    directives: [PartiesForm, RouterLink]
+    directives: [PartiesForm, RouterLink, AccountsUI]
 })
 export class PartiesList {
     parties: Mongo.Cursor<Party>;
